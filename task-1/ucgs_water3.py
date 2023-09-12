3743a1973a51a5714641d13f131b4c1e4b308264
fairytoto <144603570+fairytoto@users.noreply.github.com>
2023-09-12 15:30:00 +0700

    Update ucgs_water3.py

--- task-1/ucgs_water3.py
+++ task-1/ucgs_water3.py
@@ -2,11 +2,15 @@ import water3 as problem
 import utils
 import ucs
 
+state = (8,0,0)
+
 goal_node, n_visits = ucs.uniform_cost_graph_search(problem)
 if goal_node is not None:
+    print("")
     print("Solution")
     print("========")
     utils.print_solution(goal_node)
+    problem.successors(state)
     print("========")
     print("Path cost = %d" % goal_node[3])
     print("Number of Visited States = %d" % n_visits)
